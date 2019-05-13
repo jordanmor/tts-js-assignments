64f3ba2cd31bca8c8a3b1bc7a7c743ffa185966b
Jordan Mor <jordanmor@gmail.com>
2019-05-13 13:43:54 -0400

    Refactor 1

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 const weapons = ['rock' , 'paper', 'scissors'];
-const player1 = 'Jordan';
-const player2 = 'Computer';
-let player1NumOfWins = 0;
-let player2NumOfWins = 0;
-let player1Hand, player2Hand;
 let round = 0;
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.numOfWins = 0;
+    this.hand = undefined;
+  }
+}
+
+const player1 = new Player('Player1');
+const player2 = new Player('Player2');
 
 const weaponOfChoice = () => weapons[parseInt(Math.random()*weapons.length) %3];
 
 const compareHands = (weapon1, weapon2) => {
+  const winningHand = weapon1 === 'paper' && weapon2 === 'rock' || weapon1 === 'scissors' && weapon2 === 'paper' || weapon1 === 'rock' && weapon2 === 'scissors';
+  round++;
   if(weapon1 === weapon2) {
-    round++;
     return `Round ${round} is a Tie.`;
-  } else if (weapon1 === 'paper' && weapon2 === 'rock' || weapon1 === 'scissors' && weapon2 === 'paper' || weapon1 === 'rock' && weapon2 === 'scissors') {
-    player1NumOfWins++;
-    round++;
+  } else if (winningHand) {
+    player1.numOfWins++;
     return `Player 1 Wins Round ${round}`;
   } else {
-    player2NumOfWins++;
-    round++;
+    player2.numOfWins++;
     return `Player 2 Wins Round ${round}`;
   }
 }
 
-while(player1NumOfWins !== 3 && player2NumOfWins !== 3) {
-  player1Hand = weaponOfChoice();
-  player2Hand = weaponOfChoice();
-  console.log(compareHands(player1Hand, player2Hand));
-  if(player1NumOfWins === 3) {
+while(player1.numOfWins !== 3 && player2.numOfWins !== 3) {
+  player1.hand = weaponOfChoice();
+  player2.hand = weaponOfChoice();
+  console.log(compareHands(player1.hand, player2.hand));
+  if(player1.numOfWins === 3) {
     console.log('Player 1 Wins the Game!')
-  } else if (player2NumOfWins === 3) {
+  } else if (player1.numOfWins === 3) {
     console.log('Player 2 Wins the Game!')
   }
 }
