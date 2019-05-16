bec659b260841377a5670b7b392e5c9076fa5d67
Jordan Mor <jordanmor@gmail.com>
2019-05-16 12:04:04 -0400

    Complete JS Challenge 4 Exercise 1

---
+++ challenge04.js
@@ -0,0 +1,25 @@
+// Exercise 1
+
+function outer(){
+	let a = "horse";
+	let b = {
+		name: "Ed",
+		numOfLegs: 4
+	}
+	function inner(a, b) {
+		a = "ape";
+		b = {
+			name: "Brett",
+			numOfLegs: 2
+		};
+		b.name = "Heather";
+		console.log("Inner a:", a);
+		console.log("Inner b:", b);
+	}
+ 
+	inner(a, b);
+	console.log("Outer a:", a);
+	console.log("Outer b:", b);
+ }
+ 
+ outer();
