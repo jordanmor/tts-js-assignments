43657057a8fe975423c464773151236f89b43b15
Jordan Mor <jordanmor@gmail.com>
2019-08-23 00:56:22 -0400

    Angular Lab - Complete Animation Portion

--- angular/angular-lab/src/app/animations/animations.component.ts
+++ angular/angular-lab/src/app/animations/animations.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-animations',
   templateUrl: './animations.component.html',
-  styleUrls: ['./animations.component.css']
+  styleUrls: ['./animations.component.css'],
+  animations: [
+    trigger('shape', [
+      state('SQUARE', style({
+        height: '400px',
+        width: '400px',
+        borderRadius: '1%',
+        opacity: 1,
+        backgroundColor: 'blue'
+      })),
+     state('CIRCLE', style({
+        height: '300px',
+        width: '300px',
+        opacity: 0.4,
+        borderRadius: '100%',
+        backgroundColor: 'green'
+      })),
+      state('RECTANGLE', style({
+        height: '400px',
+        width: '800px',
+        opacity: 0.8,
+        borderRadius: '1%',
+        backgroundColor: 'firebrick'
+      })),
+      state('DOT', style({
+        height: '50px',
+        width: '50px',
+        fontSize: '6px',
+        opacity: 0.4,
+        borderRadius: '100%',
+        backgroundColor: 'gold'
+      })),
+      transition('SQUARE => CIRCLE', [animate('0.5s')]),
+      transition('SQUARE => RECTANGLE', [animate('0.5s')]),
+      transition('SQUARE => DOT', [animate('0.5s')]),
+      transition('CIRCLE => SQUARE', [animate('0.5s')]),
+      transition('CIRCLE => RECTANGLE', [animate('0.5s')]),
+      transition('CIRCLE => DOT', [animate('0.5s')]),
+      transition('RECTANGLE => SQUARE', [animate('0.5s')]),
+      transition('RECTANGLE => CIRCLE', [animate('0.5s')]),
+      transition('RECTANGLE => DOT', [animate('0.5s')]),
+      transition('DOT => SQUARE', [animate('0.5s')]),
+      transition('DOT => CIRCLE', [animate('0.5s')]),
+      transition('DOT => RECTANGLE', [animate('0.5s')]),
+    ]),
+  ]
 })
-export class AnimationsComponent implements OnInit {
+export class AnimationsComponent {
 
-  constructor() { }
+  shape: string = "SQUARE";
 
-  ngOnInit() {
+  changeShape(selection: string) {
+    enum shapes {
+      square = "SQUARE",
+      circle = "CIRCLE",
+      rectangle = "RECTANGLE",
+      dot = "DOT"
+    }
+    this.shape = selection;
+
   }
 
 }
--- angular/angular-lab/src/app/app-routing.module.ts
+++ angular/angular-lab/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { FriendsComponent } from './friends/friends.component';
 import { ProfileEditorReactiveComponent } from './profile-editor-reactive/profile-editor-reactive.component';
+import { AnimationsComponent } from './animations/animations.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'friends', component: FriendsComponent },
   { path: 'profile', component: ProfileEditorReactiveComponent },
+  { path: 'animations', component: AnimationsComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
